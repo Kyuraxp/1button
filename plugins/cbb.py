e183124dca480d352148644a4e16b383e6016a22
Kyura <96605016+Kyuraxp@users.noreply.github.com>
2022-02-17 04:04:43 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Kyuraxp/Kyura-sharingbot'>Click here</a>\n○ Channel : @Kyuraprojects\n○ Support Group : @KyuraSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://www.xnxx.com'>Click here</a>\n○ Channel : @Kyuraprojects\n○ Support Group : @KyuraSupport</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("😡 Tutup Ngentot", callback_data = "close")
+                        InlineKeyboardButton("Tutup", callback_data = "close")
                     ]
                 ]
             )
